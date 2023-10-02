7f28665e07ad58f66c569eeb44951932dbaeb2b9
Lintang Aji Yoga Pratama <lintangajiyogapratama@gmail.com>
2023-10-02 18:44:49 +0700

    adding add to cart feature

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,18 @@
 import "./product-card.styles.scss";
 
 import Button from "../button/button.component";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => {
+    addItemToCart(product);
+    console.log("test");
+  };
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={name} />
@@ -11,7 +20,9 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">ADD TO CART</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        ADD TO CART
+      </Button>
     </div>
   );
 };
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,14 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createContext } from "react";
+
+const addCartItem = (cartItems, product) => {
+  if (cartItems.find((item) => item.id === product.id)) {
+    console.log("test 3");
+    return cartItems.map((cartItem) =>
+      cartItem.id === product.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  return [...cartItems, { ...product, quantity: 1 }];
+};
 
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
-  const [isCartOpen, setIsCartOpen] = useState(true);
-  const value = { isCartOpen, setIsCartOpen };
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
+  const addItemToCart = (product) => {
+    setCartItems(addCartItem(cartItems, product));
+    console.log("test 2");
+  };
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
+
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    cartItems,
+    addItemToCart,
+    cartCount,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
